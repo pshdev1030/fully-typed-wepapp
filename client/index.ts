285917baaaa478960c0dea1e2e39458cad02c06a
pshdev1030 <pshdev1030@gmail.com>
2023-05-03 19:55:06 +0900

    feat: 클라이언트 실행코드 추가

--- client/index.ts
+++ client/index.ts
@@ -8,3 +8,17 @@ const trpc = createTRPCProxyClient<AppRouter>({
     }),
   ],
 });
+
+(async function () {
+  const userList = await trpc.userList.query();
+  console.log("userList", userList);
+
+  const createdUser = await trpc.userCreate.mutate({ name: "박성현" });
+  console.log("createdUser", createdUser);
+
+  const user = await trpc.userById.query(String(userList.length + 1));
+  console.log("user", user);
+
+  const userListAfterCreateUser = await trpc.userList.query();
+  console.log("userListAfterCreateUser", userListAfterCreateUser);
+})();
