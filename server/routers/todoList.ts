a21d6ee64745d6ad7bf4f21f5568e1e4cffd35b2
pshdev1030 <pshdev1030@gmail.com>
2023-05-09 16:55:04 +0900

    feat: default selector 추가
    SELECT * 문제를 해결하기 위해 default Selector를 추가하였습니다.
    메서드 명을 변경하였습니다.

--- server/routers/todoList.ts
+++ server/routers/todoList.ts
@@ -1,17 +1,28 @@
+import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { prisma } from "../prisma";
 import { publicProcedure, router } from "../trpc";
 
+const defaultTodoListSelect = Prisma.validator<Prisma.ToDoListSelect>()({
+  id: true,
+  title: true,
+  description: true,
+  createdAt: true,
+  updatedAt: true,
+  toDos: true,
+});
+
 export const todoListRouter = router({
-  list: publicProcedure
+  getListById: publicProcedure
     .input(
       z.object({
         id: z.string(),
       })
     )
     .query(async ({ input }) => {
-      const list = await prisma.toDoList.findFirst({
+      const list = await prisma.toDoList.findUnique({
+        select: defaultTodoListSelect,
         where: {
           id: input.id,
         },
@@ -22,5 +33,42 @@ export const todoListRouter = router({
           message: "No Todolist found.",
         });
       return list;
+    }),
+  postTodo: publicProcedure
+    .input(
+      z.object({
+        targetId: z.string(),
+        title: z.string(),
+        description: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      const { targetId, title, description } = input;
+      const targetTodoList = await prisma.toDoList.findUnique({
+        select: defaultTodoListSelect,
+        where: { id: targetId },
+      });
+
+      if (!targetTodoList) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "No Todolist found.",
+        });
+      }
+
+      const newTodo = await prisma.toDo.create({
+        data: {
+          toDoList: { connect: { id: targetTodoList.id } },
+          title,
+          description,
+        },
+      });
+
+      await prisma.toDoList.update({
+        where: { id: targetTodoList.id },
+        data: { toDos: { connect: { id: newTodo.id } } },
+      });
+
+      return newTodo;
     }),
 });
